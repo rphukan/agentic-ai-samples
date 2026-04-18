a7efec04eba7a358decb0c2513c62f1046d29fce
rphukan <ranjan.phukan@gmail.com>
2026-04-18 09:55:05 +0530

    added a github agent

--- github_agent/agent.py
+++ github_agent/agent.py
@@ -2,6 +2,8 @@ import os
 from google.adk.agents import Agent
 from google.adk.tools.mcp_tool import McpToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPServerParams
+from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
+
 
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
@@ -11,12 +13,13 @@ root_agent = Agent(
     instruction="Help users get information from GitHub",
     tools=[
         McpToolset(
-            connection_params=StreamableHTTPServerParams(
-                url="https://api.githubcopilot.com/mcp/",
+            connection_params=StreamableHTTPConnectionParams(
+                url="https://api.githubcopilot.com/mcp",
                 headers={
                     "Authorization": f"Bearer {GITHUB_TOKEN}",
                     "X-MCP-Toolsets": "all",
-                    "X-MCP-Readonly": "true"
+                    "X-MCP-Readonly": "true",
+                    "Accept": "application/json, text/event-stream"
                 },
             ),
         )
